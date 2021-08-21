fd0f8067139759ac773a1d1cd3023f1656a9ec0d
witsarutkitpanaporn <88717196+witsarutkitpanaporn@users.noreply.github.com>
2021-08-21 17:00:57 +0700

    Add files via upload

---
+++ HTML,CSS,RESPONSIVE/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.hamburger').on("click", function(){
+        $(".nav-grid").toggleClass("open");
+    });
+});
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  } 
+
